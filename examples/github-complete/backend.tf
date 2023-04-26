5f10d9a3d189d0afffca5a3efa4a712ccdd340ab
bvedor <bruno.vedor@rentcars.com>
2023-04-26 16:32:36 -0300

    rentcarsbi

--- examples/github-complete/backend.tf
+++ examples/github-complete/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-rentcars"
+    bucket = "terraform-state-rentcars-bi"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "terraform-state-lock-bi"
   }
 }
